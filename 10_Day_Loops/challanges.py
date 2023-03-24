baecc133819e22a72f41f018b78748c20dd4c987
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2023-03-24 23:50:46 +0600

    for loop challenges

--- 10_Day_Loops/challanges.py
+++ 10_Day_Loops/challanges.py
@@ -1,4 +1,10 @@
-# Challenge 1: Sum of a List
+# Challenge 1: Write a Python program to print all the even numbers from 1 to 100.
+for num in range(1, 101):
+    if num % 2 == 0:
+        print(num)
+
+
+# Challenge 2: Sum of a List
 numbers = [1, 2, 3, 4, 5]
 sum = 0
 
@@ -6,3 +12,30 @@ for num in numbers:
     sum += num
 
 print("The sum of the numbers is:", sum)
+
+# Challenge 3: Write a Python program to find the factorial of a given number.
+# Solution
+num = 5
+factorial = 1
+for i in range(1, num + 1):
+    factorial *= i
+print("Factorial of", num, "is", factorial)
+
+
+# Challenge 4: Write a Python program to print the multiplication table of a given number.
+# Solution
+num = 7
+for i in range(1, 11):
+    print(num, "x", i, "=", num * i)
+
+
+# Challenge 5: Write a Python program to find the largest element in a list.
+# Solution
+numbers = [5, 7, 3, 9, 1]
+largest = numbers[0]
+for num in numbers:
+    if num > largest:
+        largest = num
+print("Largest element in the list:", largest)
+
+
