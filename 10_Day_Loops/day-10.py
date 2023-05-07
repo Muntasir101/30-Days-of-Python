5b271e5612d280ea97a2438c5ff654a524d68bb3
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2023-05-07 23:54:32 +0600

    Loops

---
+++ 10_Day_Loops/day-10.py
@@ -0,0 +1,12 @@
+count = 0
+while count < 5:
+    print(count)
+    count = count + 1
+#prints from 0 to 4
+
+count = 0
+while count < 5:
+    print(count)
+    count = count + 1
+else:
+    print(count)
